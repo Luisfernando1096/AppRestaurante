0079128e9af42574052b17053b89e7de5f6d0f93
EdgarFuentes100 <edgarfuentes139@gmail.com>
2023-11-16 23:41:00 -0300

    Enviando comanda completa a la app de escritorio para imprimir

--- app/src/main/java/com/example/apprestaurante/ComandaGestion.java
+++ app/src/main/java/com/example/apprestaurante/ComandaGestion.java
@@ -201,6 +201,7 @@ public class ComandaGestion extends AppCompatActivity implements  PedidosAdapter
         if (tag.equals("1")) {
             //Comanda
             Toast.makeText(this, "Click en comandas", Toast.LENGTH_SHORT).show();
+            EnviarListaCompleta();
         } else if (tag.equals("2")) {
             //Extra
             Intent intent = new Intent(ComandaGestion.this, DividirPedidos.class);
@@ -950,8 +951,34 @@ public class ComandaGestion extends AppCompatActivity implements  PedidosAdapter
     }
     private void Enviar(){
         enviarListaTask.enviarLista(lstPD);
+        lstPD.size();
     }
-
+    private void EnviarListaCompleta(){
+        for (PedidoDetalle pDetalle : lstPedidos) {
+            if(cliente != null){
+                pDetalle.setCliente(cliente);
+            }else{
+                pDetalle.setCliente("");
+            }
+            if(mesero != null){
+                pDetalle.setMesero(mesero);
+            }else{
+                pDetalle.setMesero("");
+            }
+            if(salon != null){
+                pDetalle.setSalon(salon);
+            }else{
+                pDetalle.setSalon("");
+            }
+            if(mesa != null){
+                pDetalle.setMesa(mesa);
+            }else{
+                pDetalle.setMesa("");
+            }
+        }
+        enviarListaTask.enviarListaCompleta(lstPedidos);
+        lstPedidos.size();
+    }
     private void ObtenerPedidoPorId(String id){
         PedidoService pedidoService = new PedidoService();
 
--- app/src/main/java/com/example/apprestaurante/utils/EnviarListaTask.java
+++ app/src/main/java/com/example/apprestaurante/utils/EnviarListaTask.java
@@ -1,7 +1,5 @@
 package com.example.apprestaurante.utils;
-
 import android.os.AsyncTask;
-
 import com.example.apprestaurante.ComandaGestion;
 import com.example.apprestaurante.clases.PedidoDetalle;
 import com.example.apprestaurante.services.ApiService;
@@ -10,16 +8,13 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.List;
-
 import android.os.AsyncTask;
 import android.widget.Toast;
-
 import com.google.gson.Gson;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.List;
-
 import com.google.gson.Gson;
 import okhttp3.MediaType;
 import okhttp3.RequestBody;
@@ -34,7 +29,7 @@ public class EnviarListaTask {
     public void enviarLista(List<PedidoDetalle> lista) {
         // Crear instancia de Retrofit
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://192.168.2.105:4000/") // URL base de tu servicio
+                .baseUrl("http://192.168.233.52:4000/") // URL base de tu servicio
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
@@ -70,7 +65,38 @@ public class EnviarListaTask {
             }
         });
     }
+
+    public void enviarListaCompleta(List<PedidoDetalle> lista) {
+        // Crear instancia de Retrofit
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://192.168.233.52:4000/") // URL base de tu servicio
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        // Crear la interfaz del servicio
+        ApiService apiService = retrofit.create(ApiService.class);
+        // Serializar la lista de objetos a JSON
+        Gson gson = new Gson();
+        String jsonData = gson.toJson(lista);
+        System.out.println("datos: " + jsonData);
+        // Crear el cuerpo de la solicitud
+        RequestBody requestBody = RequestBody.create(MediaType.parse("application/json"), jsonData);
+        // Hacer la llamada a la API utilizando Retrofit
+        Call<Void> call = apiService.enviarListaCompleta(requestBody);
+        call.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                if (response.isSuccessful()) {
+                    // Manejar la respuesta del servidor según sea necesario
+                    // Puedes leer el contenido de la respuesta aquí si es necesario
+                } else {
+                    // Manejar un código de respuesta no exitoso si es necesario
+                }
+            }
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                // Manejar el fallo en la comunicación con el servidor
+                t.printStackTrace();
+            }
+        });
+    }
 }
-
-
-
