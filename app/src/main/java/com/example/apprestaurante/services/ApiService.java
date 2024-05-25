5ef7443c0d3f2089b3ac3b1b0e7300e7c827852e
fruiz <fruiz@LAPTOP-QBB4AB87>
2024-05-24 22:09:04 -0600

    Se modifico para gestionar respuesta del servidor al imprimir

--- app/src/main/java/com/example/apprestaurante/services/ApiService.java
+++ app/src/main/java/com/example/apprestaurante/services/ApiService.java
@@ -1,6 +1,7 @@
 package com.example.apprestaurante.services;
 
 import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.POST;
@@ -8,12 +9,12 @@ import retrofit2.http.POST;
 public interface ApiService {
 
     @POST("notificar") // Ruta relativa al baseUrl
-    Call<Void> enviarLista(@Body RequestBody body);
+    Call<ResponseBody> enviarLista(@Body RequestBody body);
 
     @POST("comandaCompleta") // Ruta relativa al baseUrl
-    Call<Void> enviarListaCompleta(@Body RequestBody body);
+    Call<ResponseBody> enviarListaCompleta(@Body RequestBody body);
 
     @POST("datosPrecuenta") // Ruta relativa al baseUrl
-    Call<Void> enviarInformacion(@Body RequestBody body);
+    Call<ResponseBody> enviarInformacion(@Body RequestBody body);
 }
 
--- app/src/main/java/com/example/apprestaurante/utils/EnviarListaTask.java
+++ app/src/main/java/com/example/apprestaurante/utils/EnviarListaTask.java
@@ -8,10 +8,12 @@ import com.example.apprestaurante.network.ApiClient;
 import com.example.apprestaurante.services.ApiService;
 import com.google.gson.Gson;
 
+import java.io.IOException;
 import java.util.List;
 
 import okhttp3.MediaType;
 import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -45,25 +47,34 @@ public class EnviarListaTask {
         RequestBody requestBody = RequestBody.create(MediaType.parse("application/json"), jsonData);
 
         // Hacer la llamada a la API utilizando Retrofit
-        Call<Void> call = apiService.enviarLista(requestBody);
-        call.enqueue(new Callback<Void>() {
+        Call<ResponseBody> call = apiService.enviarLista(requestBody);
+        call.enqueue(new Callback<ResponseBody>() {
             @Override
-            public void onResponse(Call<Void> call, Response<Void> response) {
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
                 if (response.isSuccessful()) {
-                    // Manejar la respuesta del servidor según sea necesario
-                    // Puedes leer el contenido de la respuesta aquí si es necesario
+                    try {
+                        String responseBody = response.body().string();
+                        if ("true".equals(responseBody)) {
+                            Toast.makeText(context, "Finalizado con éxito", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(context, "Error en la respuesta del servidor", Toast.LENGTH_SHORT).show();
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        Toast.makeText(context, "Error al leer la respuesta del servidor", Toast.LENGTH_SHORT).show();
+                    }
                 } else {
-                    // Manejar un código de respuesta no exitoso si es necesario
+                    Toast.makeText(context, "Código de respuesta no exitoso: " + response.code(), Toast.LENGTH_SHORT).show();
                 }
                 progressDialog.dismiss();
-                Toast.makeText(context, "Finalizado con exito", Toast.LENGTH_SHORT).show();
             }
 
             @Override
-            public void onFailure(Call<Void> call, Throwable t) {
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
                 progressDialog.dismiss();
                 // Manejar el fallo en la comunicación con el servidor
                 t.printStackTrace();
+                Toast.makeText(context, "Fallo en la comunicación con el servidor", Toast.LENGTH_SHORT).show();
             }
         });
     }
@@ -81,27 +92,38 @@ public class EnviarListaTask {
         // Crear el cuerpo de la solicitud
         RequestBody requestBody = RequestBody.create(MediaType.parse("application/json"), jsonData);
         // Hacer la llamada a la API utilizando Retrofit
-        Call<Void> call = apiService.enviarListaCompleta(requestBody);
-        call.enqueue(new Callback<Void>() {
+        Call<ResponseBody> call = apiService.enviarListaCompleta(requestBody);
+        call.enqueue(new Callback<ResponseBody>() {
             @Override
-            public void onResponse(Call<Void> call, Response<Void> response) {
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
                 if (response.isSuccessful()) {
-                    // Manejar la respuesta del servidor según sea necesario
-                    // Puedes leer el contenido de la respuesta aquí si es necesario
+                    try {
+                        String responseBody = response.body().string();
+                        if ("true".equals(responseBody)) {
+                            Toast.makeText(context, "Finalizado con éxito", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(context, "Error en la respuesta del servidor", Toast.LENGTH_SHORT).show();
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        Toast.makeText(context, "Error al leer la respuesta del servidor", Toast.LENGTH_SHORT).show();
+                    }
                 } else {
-                    // Manejar un código de respuesta no exitoso si es necesario
+                    Toast.makeText(context, "Código de respuesta no exitoso: " + response.code(), Toast.LENGTH_SHORT).show();
                 }
                 progressDialog.dismiss();
-                Toast.makeText(context, "Finalizado con exito", Toast.LENGTH_SHORT).show();
             }
+
             @Override
-            public void onFailure(Call<Void> call, Throwable t) {
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
                 progressDialog.dismiss();
                 // Manejar el fallo en la comunicación con el servidor
                 t.printStackTrace();
+                Toast.makeText(context, "Fallo en la comunicación con el servidor", Toast.LENGTH_SHORT).show();
             }
         });
     }
+
 
     public void enviarTotalPedido(List<PedidoDetalle> lista) {
         progressDialog.show();
@@ -116,24 +138,34 @@ public class EnviarListaTask {
         // Crear el cuerpo de la solicitud
         RequestBody requestBody = RequestBody.create(MediaType.parse("application/json"), jsonData);
         // Hacer la llamada a la API utilizando Retrofit
-        Call<Void> call = apiService.enviarInformacion(requestBody);
-        call.enqueue(new Callback<Void>() {
+        Call<ResponseBody> call = apiService.enviarInformacion(requestBody);
+        call.enqueue(new Callback<ResponseBody>() {
             @Override
-            public void onResponse(Call<Void> call, Response<Void> response) {
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
                 if (response.isSuccessful()) {
-                    // Manejar la respuesta del servidor según sea necesario
-                    // Puedes leer el contenido de la respuesta aquí si es necesario
+                    try {
+                        String responseBody = response.body().string();
+                        if ("true".equals(responseBody)) {
+                            Toast.makeText(context, "Finalizado con éxito", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(context, "Error en la respuesta del servidor", Toast.LENGTH_SHORT).show();
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        Toast.makeText(context, "Error al leer la respuesta del servidor", Toast.LENGTH_SHORT).show();
+                    }
                 } else {
-                    // Manejar un código de respuesta no exitoso si es necesario
+                    Toast.makeText(context, "Código de respuesta no exitoso: " + response.code(), Toast.LENGTH_SHORT).show();
                 }
                 progressDialog.dismiss();
-                Toast.makeText(context, "Finalizado con exito", Toast.LENGTH_SHORT).show();
             }
+
             @Override
-            public void onFailure(Call<Void> call, Throwable t) {
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
                 progressDialog.dismiss();
                 // Manejar el fallo en la comunicación con el servidor
                 t.printStackTrace();
+                Toast.makeText(context, "Fallo en la comunicación con el servidor", Toast.LENGTH_SHORT).show();
             }
         });
     }
